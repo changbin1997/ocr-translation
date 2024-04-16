fb27cb0a5e4bfa735a67069d99f46f3a852259c6
changbin1997 <zsmcbdyx@foxmail.com>
2024-04-16 23:43:18 +0800

    调整了代码结构，拆分了一些模块
    1. src/electron-main.js 入口文件的一些事件和方法拆分为独立的模块
    2. src/modules/ipc.js 改为 MyApp.js，加入了一些之前在入口文件的方法

--- src/electron-main.js
+++ src/electron-main.js
@@ -1,14 +1,9 @@
-const { app, BrowserWindow, globalShortcut, dialog, Menu, Tray, clipboard } = require('electron');
+const { app, BrowserWindow, globalShortcut, Menu } = require('electron');
 const path = require('path');
-const ScreenshotOcr = require('./modules/screenshotOcr'); // 截图模块
-const Data = require('./modules/Data'); // 数据库操作模块
-
-// 数据库初始化
-const data = new Data();
+const MyApp = require('./modules/MyApp');
 
 let mainWindow; // 用来保存主窗口对象的引用
-let disabled = false; // 用于禁用截图识别
-let tray = null;  // 用来存储系统托盘
+const myApp = new MyApp();
 
 // 当 Electron 完成初始化并准备创建浏览器窗口时被调用
 app.on('ready', async () => {
@@ -46,185 +41,20 @@ app.on('ready', async () => {
     mainWindow = null;
   });
 
-  // 如果无法打开数据库
-  if (data.dbErr !== null) {
-    await dialog.showMessageBox(mainWindow, {
-      title: '无法初始化数据库',
-      message: '无法打开和初始化数据库，程序无法正常运行！',
-      type: 'error',
-      buttons: ['退出程序'],
-      noLink: true
-    });
-    // 退出软件
-    app.quit();
-  }
-
-  // 数据库初始化
-  const initResult = await data.init();
-  // 出错
-  if (initResult.result !== 'success') {
-    await dialog.showMessageBox(mainWindow, {
-      title: '数据库初始化错误',
-      message: initResult.msg,
-      type: 'error',
-      buttons: ['退出程序'],
-      noLink: true
-    });
-    // 退出软件
-    app.quit();
-  }
-
-  // 初始化快捷键截图识别
-  let options = await data.getOptions();
-  // 出错
-  if (options.result !== 'success') {
-    await dialog.showMessageBox(mainWindow, {
-      title: '查询数据出错！',
-      message: options.msg,
-      type: 'error',
-      buttons: ['退出程序'],
-      noLink: true
-    });
-    // 退出软件
-    app.quit();
-  }
-  const screenshotOcr = new ScreenshotOcr(options.options);
-  // 如果开启了全局快捷键1
-  if (options.options.key1Enable) {
-    // 快捷键1事件
-    globalShortcut.register(options.options.key1Name, async () => {
-      if (disabled) return false;
-      disabled = true; // 正在识别时禁用截图
-      const result = await screenshotOcr.ocr(
-        options.options.key1Provider,
-        options.options.key1Function
-      );
-      disabled = false; // 识别完成后恢复截图
-      // 取消截图
-      if (result === null) return false;
-      if (result.result !== 'success') {
-        await dialog.showMessageBox(mainWindow, {
-          title:'OCR 识别出错',
-          message: result.msg,
-          type: 'error',
-          buttons: ['关闭'],
-          noLink: true
-        });
-        return false;
-      }
-      result.auto = options.options.key1Auto;
-      mainWindow.webContents.send('ocrResult', result);
-    });
-  }
-
-  // 如果开启了全局快捷键2
-  if (options.options.key2Enable) {
-    // 快捷键2事件
-    globalShortcut.register(options.options.key2Name, async () => {
-      if (disabled) return false;
-      disabled = true; // 正在识别时禁用截图
-      const result = await screenshotOcr.ocr(
-        options.options.key2Provider,
-        options.options.key2Function
-      );
-      disabled = false; // 识别完成后恢复截图
-      // 取消截图
-      if (result === null) return false;
-      if (result.result !== 'success') {
-        await dialog.showMessageBox(mainWindow, {
-          title: 'OCR 识别出错',
-          message: result.msg,
-          type: 'error',
-          buttons: ['关闭'],
-          noLink: true
-        });
-        return false;
-      }
-      result.auto = options.options.key2Auto;
-      mainWindow.webContents.send('ocrResult', result);
-    });
-  }
-
-  // 如果开启了指定区域识别
-  if (options.options.specificArea) {
-    globalShortcut.register(options.options.specificAreaKeyName, async () => {
-      if (disabled) return false;
-      disabled = true; // 正在识别时禁用截图
-      // 调用截取指定区域识别
-      const result = await screenshotOcr.specificArea(
-        options.options.specificAreaProvider,
-        options.options.specificAreaApi,
-        options.options.specificAreaLeft,
-        options.options.specificAreaTop,
-        options.options.specificAreaWidth,
-        options.options.specificAreaHeight
-      );
-      disabled = false; // 识别完成后恢复截图
-      // 识别出错
-      if (result.result !== 'success') {
-        await dialog.showMessageBox(mainWindow, {
-          title: 'OCR 识别出错',
-          message: result.msg,
-          type: 'error',
-          buttons: ['关闭'],
-          noLink: true
-        });
-        return false;
-      }
-      result.auto = options.options.specificAreaAuto;
-      mainWindow.webContents.send('ocrResult', result);
-    });
-  }
-
-  // 如果开启了剪贴板翻译
-  if (options.options.clipboardTranslation) {
-    globalShortcut.register(options.options.clipboardTranslationKeyName, () => {
-      // 读取剪贴板的文字
-      const clipboardText = clipboard.readText();
-      if (clipboardText === '') return false;
-      // 把剪贴板的内容传给渲染进程的 app 组件
-      mainWindow.webContents.send('clipboardTranslation', clipboardText);
-    });
-  }
-
-  // 创建托盘图标菜单
-  tray = new Tray(path.join(__dirname, 'icon.ico'));
-  // 菜单模板
-  const trayMenu = [
-    {
-      label: '显示主窗口',
-      click: () => {
-        if (mainWindow.isVisible()) return false;
-        mainWindow.show();
-      }
-    },
-    {
-      label: '设置',
-      click: () => {
-        mainWindow.webContents.send('toPage', 'optionsPage');
-        if (!mainWindow.isVisible()) mainWindow.show();
-      }
-    },
-    {
-      label: '退出',
-      role: 'quit'
-    }
-  ];
-  // 创建菜单
-  tray.setContextMenu(Menu.buildFromTemplate(trayMenu));
-  // 托盘图标设置提示文字
-  tray.setToolTip(`OCR识别翻译\n${options.options.key1Name}：${options.options.key1Function}\n${options.options.key2Name}：${options.options.key2Function}`);
-  // 托盘图标点击
-  tray.on('click', () => {
-    if (mainWindow.isVisible()) return false;
-    mainWindow.show();
-  });
-
   // 窗口最小化
   mainWindow.on('minimize', () => {
     mainWindow.hide();
   });
+
+  // 初始化数据库
+  await myApp.dataInit(mainWindow);
+
+  // 全局快捷键
+  await myApp.globalShortcut(mainWindow);
+
+  // 托盘图标菜单
+  await myApp.trayMenu(mainWindow);
 });
 
-// 用于处理 IPC 请求
-require('./modules/ipc');
+// 处理 ipc 请求
+myApp.ipc();
---
+++ src/modules/MyApp.js
@@ -0,0 +1,339 @@
+const {dialog, ipcMain, BrowserWindow, clipboard, shell, app, globalShortcut, Tray, Menu} = require('electron');
+const Ocr = require('./Ocr');  // OCR 模块
+const BaiduTranslation = require('./BaiduTranslation');  // 百度翻译模块
+const Data = require('./Data');  // 数据库操作模块
+const ContextMenu = require('./contextMenu');  // 上下文菜单模块
+const selectorWindow = require('./selector-window');
+const ScreenshotOcr = require('./screenshotOcr');
+const path = require("path"); // 截图模块
+let disabled = false; // 用于禁用截图识别
+let tray = null;  // 用来存储系统托盘
+
+module.exports = class MyApp {
+  data = null;
+  options = null;
+
+  // 数据库初始化
+  async dataInit(mainWindow) {
+    this.data = new Data();
+    // 如果无法打开数据库
+    if (this.data.dbErr !== null) {
+      await dialog.showMessageBox(mainWindow, {
+        title: '无法初始化数据库',
+        message: '无法打开和初始化数据库，程序无法正常运行！',
+        type: 'error',
+        buttons: ['退出程序'],
+        noLink: true
+      });
+      // 退出软件
+      app.quit();
+    }
+
+    // 数据库初始化
+    const initResult = await this.data.init();
+    // 出错
+    if (initResult.result !== 'success') {
+      await dialog.showMessageBox(mainWindow, {
+        title: '数据库初始化错误',
+        message: initResult.msg,
+        type: 'error',
+        buttons: ['退出程序'],
+        noLink: true
+      });
+      // 退出软件
+      app.quit();
+    }
+
+    // 获取设置
+    this.options = await this.data.getOptions();
+    // 出错
+    if (this.options.result !== 'success') {
+      await dialog.showMessageBox(mainWindow, {
+        title: '查询数据出错！',
+        message: this.options.msg,
+        type: 'error',
+        buttons: ['退出程序'],
+        noLink: true
+      });
+      // 退出软件
+      app.quit();
+    }
+  }
+
+  // 托盘图标菜单
+  async trayMenu(mainWindow) {
+    tray = new Tray(path.normalize(path.join(__dirname, '../icon.ico')));
+    // 菜单模板
+    const trayMenu = [
+      {
+        label: '显示主窗口',
+        click: () => {
+          if (mainWindow.isVisible()) return false;
+          mainWindow.show();
+        }
+      },
+      {
+        label: '设置',
+        click: () => {
+          mainWindow.webContents.send('toPage', 'optionsPage');
+          if (!mainWindow.isVisible()) mainWindow.show();
+        }
+      },
+      {
+        label: '退出',
+        role: 'quit'
+      }
+    ];
+    // 创建菜单
+    tray.setContextMenu(Menu.buildFromTemplate(trayMenu));
+    // 托盘图标设置提示文字
+    tray.setToolTip(`OCR识别翻译\n${this.options.options.key1Name}：${this.options.options.key1Function}\n${this.options.options.key2Name}：${this.options.options.key2Function}`);
+    // 托盘图标点击
+    tray.on('click', () => {
+      if (mainWindow.isVisible()) return false;
+      mainWindow.show();
+    });
+  }
+
+  // 全局快捷键
+  async globalShortcut(mainWindow) {
+    const screenshotOcr = new ScreenshotOcr(this.options.options);
+    // 如果开启了全局快捷键1
+    if (this.options.options.key1Enable) {
+      // 快捷键1事件
+      globalShortcut.register(this.options.options.key1Name, async () => {
+        if (disabled) return false;
+        disabled = true; // 正在识别时禁用截图
+        const result = await screenshotOcr.ocr(
+          this.options.options.key1Provider,
+          this.options.options.key1Function
+        );
+        disabled = false; // 识别完成后恢复截图
+        // 取消截图
+        if (result === null) return false;
+        if (result.result !== 'success') {
+          await dialog.showMessageBox(mainWindow, {
+            title:'OCR 识别出错',
+            message: result.msg,
+            type: 'error',
+            buttons: ['关闭'],
+            noLink: true
+          });
+          return false;
+        }
+        result.auto = this.options.options.key1Auto;
+        mainWindow.webContents.send('ocrResult', result);
+      });
+    }
+
+    // 如果开启了全局快捷键2
+    if (this.options.options.key2Enable) {
+      // 快捷键2事件
+      globalShortcut.register(this.options.options.key2Name, async () => {
+        if (disabled) return false;
+        disabled = true; // 正在识别时禁用截图
+        const result = await screenshotOcr.ocr(
+          this.options.options.key2Provider,
+          this.options.options.key2Function
+        );
+        disabled = false; // 识别完成后恢复截图
+        // 取消截图
+        if (result === null) return false;
+        if (result.result !== 'success') {
+          await dialog.showMessageBox(mainWindow, {
+            title: 'OCR 识别出错',
+            message: result.msg,
+            type: 'error',
+            buttons: ['关闭'],
+            noLink: true
+          });
+          return false;
+        }
+        result.auto = this.options.options.key2Auto;
+        mainWindow.webContents.send('ocrResult', result);
+      });
+    }
+
+    // 如果开启了指定区域识别
+    if (this.options.options.specificArea) {
+      globalShortcut.register(this.options.options.specificAreaKeyName, async () => {
+        if (disabled) return false;
+        disabled = true; // 正在识别时禁用截图
+        // 调用截取指定区域识别
+        const result = await screenshotOcr.specificArea(
+          this.options.options.specificAreaProvider,
+          this.options.options.specificAreaApi,
+          this.options.options.specificAreaLeft,
+          this.options.options.specificAreaTop,
+          this.options.options.specificAreaWidth,
+          this.options.options.specificAreaHeight
+        );
+        disabled = false; // 识别完成后恢复截图
+        // 识别出错
+        if (result.result !== 'success') {
+          await dialog.showMessageBox(mainWindow, {
+            title: 'OCR 识别出错',
+            message: result.msg,
+            type: 'error',
+            buttons: ['关闭'],
+            noLink: true
+          });
+          return false;
+        }
+        result.auto = this.options.options.specificAreaAuto;
+        mainWindow.webContents.send('ocrResult', result);
+      });
+    }
+
+    // 如果开启了剪贴板翻译
+    if (this.options.options.clipboardTranslation) {
+      globalShortcut.register(this.options.options.clipboardTranslationKeyName, () => {
+        // 读取剪贴板的文字
+        const clipboardText = clipboard.readText();
+        if (clipboardText === '') return false;
+        // 把剪贴板的内容传给渲染进程的 app 组件
+        mainWindow.webContents.send('clipboardTranslation', clipboardText);
+      });
+    }
+  }
+
+
+  // 处理渲染进程的 ipc 请求
+  ipc() {
+    // 打开屏幕区域选择窗口
+    ipcMain.handle('selector-window',  async () => {
+      return await selectorWindow();
+    });
+
+    // 重启程序
+    ipcMain.handle('restart', () => {
+      app.relaunch();
+      app.exit();
+    });
+
+    // 显示对话框
+    ipcMain.handle('dialog', async (ev, args) => {
+      return dialog[args.name](BrowserWindow.getFocusedWindow(), args.options);
+    });
+
+    // 是否是图片文件
+    ipcMain.handle('isImage',  (ev, args) => {
+      return Ocr.isImage(args);
+    });
+
+    // 图片转 base64
+    ipcMain.handle('fileToBase64', (ev, args) => {
+      return Ocr.fileToBase64(args);
+    });
+
+    // OCR识别
+    ipcMain.handle('ocr', async (ev, args) => {
+      const ocr = new Ocr(args.options);
+      // 是否是讯飞 OCR
+      if (args.provider === 'xunfei') {
+        return await ocr.xunfei(args.type, args.base64File, args.imgType);
+      }else {
+        return await ocr[args.provider](args.type, args.base64File);
+      }
+    });
+
+    // 拷贝文本到剪贴板
+    ipcMain.handle('copy-text', (ev, args) => {
+      clipboard.writeText(args);
+    });
+
+    // 百度翻译
+    ipcMain.handle('translation', async (ev, args) => {
+      const baiduTranslation = new BaiduTranslation(args.options);
+      return await baiduTranslation.send(args.q, args.from, args.to);
+    });
+
+    // 获取选项
+    ipcMain.handle('getOptions', async () => {
+      return this.options;
+    });
+
+    // 保存选项
+    ipcMain.handle('updateOptions', async (ev, args) => {
+      return await this.data.updateOptions(args);
+    });
+
+    // 获取 OCR 记录总览
+    ipcMain.handle('ocrHistoryOverview', async () => {
+      const baiduData = await this.data.getBaiduOcrHistoryOverview();
+      const tencentData = await this.data.getTencentOcrHistoryOverview();
+      const xunfeiData = await this.data.getXunfeiOcrHistoryOverview();
+      const youdaoData = await this.data.getYoudaoOcrHistoryOverview();
+
+      return {
+        baidu: baiduData,
+        tencent: tencentData,
+        xunfei: xunfeiData,
+        youdao: youdaoData
+      };
+    });
+
+    // 获取 OCR 历史记录
+    ipcMain.handle('ocrHistoryList',  async (ev, args) => {
+      return await this.data.getOcrHistoryList(args.start, args.count);
+    });
+
+    // 获取翻译数据总览
+    ipcMain.handle('translationHistoryOverview', async () => {
+      return await this.data.getTranslationHistoryOverview();
+    });
+
+    // 获取翻译历史记录数据
+    ipcMain.handle('translationHistoryList', async (ev, args) => {
+      return await this.data.getTranslationHistoryList(args.start, args.count);
+    });
+
+    // 清空翻译历史记录
+    ipcMain.handle('deleteAllTranslationHistory', async () => {
+      return await this.data.deleteAllTranslationHistory();
+    });
+
+    // 清空指定提供商的 OCR 历史记录
+    ipcMain.handle('deleteOcrHistory', async (ev, args) => {
+      return await this.data.deleteAllOcrHistory(args);
+    });
+
+    // 上下文菜单请求
+    ipcMain.on('contextMenu', (ev, args) => {
+      // 弹出上下文菜单
+      ContextMenu.inputMenu(args.x, args.y);
+    });
+
+    // 导出翻译结果菜单请求
+    ipcMain.on('exportTranslationMenu', (ev, args) => {
+      // 弹出用于导出翻译结果的菜单
+      ContextMenu.exportTranslationMenu(args.x, args.y, args.result);
+    });
+
+    // 导出 OCR 识别结果菜单请求
+    ipcMain.on('exportOcrMenu', (ev, args) => {
+      // 弹出用于导出翻译结果的菜单
+      ContextMenu.exportOcrMenu(args.x, args.y, args.result);
+    });
+
+    // 通过浏览器打开链接地址
+    ipcMain.on('openLink', (ev, args) => {
+      shell.openExternal(args);
+    });
+
+    // 添加收藏
+    ipcMain.handle('addToFavorites', async (ev, args) => {
+      return await this.data.addToFavorites(args);
+    });
+
+    // 获取收藏
+    ipcMain.handle('getFavorites', async (ev, args) => {
+      return await this.data.getFavorites(args);
+    });
+
+    // 删除收藏
+    ipcMain.handle('deleteFavorite', async (ev, args) => {
+      return await this.data.deleteFavorite(args);
+    });
+  }
+};
